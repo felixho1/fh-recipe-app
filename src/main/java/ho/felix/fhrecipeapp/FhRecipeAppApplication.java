5b7444ad1656832e0f3e3afbee72a8a111067853
Felix Ho <felix.ho@nab.com.au>
2018-09-28 16:07:46 +1000

    Add .gitignore and mvn wrapper

---
+++ src/main/java/ho/felix/fhrecipeapp/FhRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package ho.felix.fhrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhRecipeAppApplication.class, args);
+    }
+}
