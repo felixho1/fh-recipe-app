9e9b342056da3992ccb24efe010d1d6026b8bbe0
Felix Ho <felix.ho@nab.com.au>
2018-10-01 21:23:54 +1000

    OneToOne, OneToMany, ManyToOne

---
+++ src/main/java/ho/felix/fhrecipeapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package ho.felix.fhrecipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
