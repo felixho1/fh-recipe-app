486cd1482ec95b694580c583a61a63ab97c64e99
Felix Ho <felix.ho@nab.com.au>
2018-10-02 10:44:48 +1000

    JPA Query Method check-in 2

--- src/main/java/ho/felix/fhrecipeapp/controllers/IndexController.java
+++ src/main/java/ho/felix/fhrecipeapp/controllers/IndexController.java
@@ -1,12 +1,26 @@
 package ho.felix.fhrecipeapp.controllers;
 
+import ho.felix.fhrecipeapp.repositories.CategoryRepository;
+import ho.felix.fhrecipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        System.out.println(categoryRepository.findByDescription("Italian").get().getId());
+        System.out.println(categoryRepository.findByDescription("Italian").get().getDescription());
+        System.out.println(unitOfMeasureRepository.findByDescription("Cup").get().getId());
+        System.out.println(unitOfMeasureRepository.findByDescription("Cup").get().getDescription());
         return "index";
     }
 
